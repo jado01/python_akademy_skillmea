9ae1a641de950087f294fc1a56b6648a454f4813
jado01 <jan.dorinsky@protonmail.com>
2025-09-28 10:16:16 +0200

    solve: add solution for webinar_08 task_set1 (05 - find max number)

--- webinar_08/examples/task_set01/task_05_max_number.py
+++ webinar_08/examples/task_set01/task_05_max_number.py
@@ -4,3 +4,28 @@
 # Vystup: Najväčšie číslo je 14
 
 # Sem napíš svoje riešenie:
+
+# RIESENIE S FOR CYKLOM:
+
+# numbers = [3, 9, 1, 14, 7]
+
+# max_number = numbers[0]
+
+# for number in numbers:
+#     if number > max_number:
+#         max_number = number
+# print(max_number)
+
+# RIESENIE S WHILE CYKLOM:
+
+numbers = [3, 9, 1, 14, 7]
+
+max_number = numbers[0]
+index = 0
+
+while index < len(numbers):
+    if numbers[index] > max_number:
+        max_number = numbers[index]
+    index += 1
+print(max_number)
+
