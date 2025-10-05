0460809eb307ade3dc1ad597a4056e6ce2bd5170
jado01 <jan.dorinsky@protonmail.com>
2025-10-06 01:16:36 +0200

    Initial working version of Simple Notes Manager with menu, main loop and reading notes from file

--- project_01_simple_notes_manager/main.py
+++ project_01_simple_notes_manager/main.py
@@ -1,17 +1,59 @@
-menu = "" \
-"Simple Notes Manager\n" \
-"====================\n" \
-"1. List Notes\n" \
-"2. Add a Note\n" \
-"3. Delete a Note\n" \
-"4. Exit\n" \
+import os
 
-print(menu)
+def clear_terminal():
+    if os.name == "nt":
+        os.system("cls")
+    elif os.name == "posix":
+        os.system("clear")
+
+def show_menu():
+    print("""Simple Notes Manager
+====================
+1.  List Notes
+2.  Add a Note
+3.  Delete a Note
+4.  Exit""")
+
+def get_choice():
+    while True:
+        choice = input("Choose an option (1-4): ").strip()
+        if choice in {"1", "2", "3", "4"}:
+            return choice
+        else:
+            print("Neplatna volba, skus 1-4")
+
+def handle_list():
+    print("List (WIP)")
+    data = read_notes()
+    if data.strip() == "":
+        print("Neexistuju ziadne poznamky")
+    else:
+        blocks = data.split("--------------------")
+        for block in blocks:
+            print(">>>NOvy BLOK<<")
+            print(block)
+            print("--------------------")
+
+def read_notes():
+    try:
+        with open("notes.txt", "r", encoding="utf-8") as file:
+            data = file.read()
+            return data
+    except FileNotFoundError:
+        return ""
 
 while True:
-    option = int(input("Choose an option (1?4):\n"))
-    if option == 3:
-        delete_notes = print("Enter the ID of the note to delete:")
-    elif option == 4:
-        print("Goodbye!")
+    clear_terminal()
+
+    show_menu()
+
+    choice = get_choice()
+    if choice == "1":
+        handle_list()
+        input("Stlac enter")
+    elif choice == "2":
+        print("ADD note")
+    elif choice == "3":
+        print("Delete a note")
+    else:
         break
