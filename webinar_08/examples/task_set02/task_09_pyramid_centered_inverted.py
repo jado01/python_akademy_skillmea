9b1ecb806e59b745f63a28a2fd9ee1ee9f543a2c
jado01 <jan.dorinsky@protonmail.com>
2026-06-07 10:54:35 +0200

    Add solutions webinar_08/task_set02 task 9 and 10

--- webinar_08/examples/task_set02/task_09_pyramid_centered_inverted.py
+++ webinar_08/examples/task_set02/task_09_pyramid_centered_inverted.py
@@ -2,3 +2,11 @@
 # Zadaj n a vytvor obrátenú centrovanú pyramídu.
 
 # Sem napíš svoje riešenie:
+n = 4
+
+for row in range(n, 0, -1):
+    for column in range(0, n - row):
+        print(" ", end="")
+    for star in range(row * 2 - 1, 0, -1):
+        print("*", end="")
+    print()
--- webinar_08/examples/task_set02/task_10_spiral.py
+++ webinar_08/examples/task_set02/task_10_spiral.py
@@ -2,3 +2,23 @@
 # Zadaj n a vytvor špirálu čísel n x n.
 
 # Sem napíš svoje riešenie:
+
+while number <= n * n:
+    grid[row][column] = number
+    number += 1
+
+    next_row = row + direction[aktual_direction][0]
+    next_column = column + direction[aktual_direction][1]
+    
+    if (next_row < 0 or next_row >= n or next_column < 0 or next_column >= n or grid[next_row][next_column] != 0):
+        aktual_direction = (aktual_direction + 1) % 4
+
+        row = row + direction[aktual_direction][0]
+        column = column + direction[aktual_direction][1]
+
+    else:
+        row = next_row
+        column = next_column
+
+for r in grid:
+    print(r)
