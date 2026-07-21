6fd77c1c810641181e5097f5b91503743e148aec
jado01 <jan.dorinsky@protonmail.com>
2026-07-21 11:20:26 +0200

    add salary change logging

--- projects/02_employee_management/main.py
+++ projects/02_employee_management/main.py
@@ -1,18 +1,29 @@
+import datetime
+from pathlib import Path
+
+def save_log(message):
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(LOG_FILE, "a", encoding="UTF-8") as f:
+        f.write(f"[{time_stamp}] {message}\n")
+
 class Employee:
-    def __init__(self, name, position, salary):
+    def __init__(self, name, surname, position, salary):
         self.name = name
+        self.surname = surname
         self.position = position
         self.salary = salary
 
-    def raise_salary(self, amount):
-        if not isinstance(amount, int):
+    def raise_salary(self, increase_amount):
+        if not isinstance(increase_amount, int):
             raise TypeError("Amount must be a number")
-        if amount <= 0:
+        if increase_amount <= 0:
             raise ValueError("Amount must be > 0")
-        self.salary += amount
+        old_salary = self.salary
+        self.salary += increase_amount
+        save_log(f"Salary increased for {self.name} {self.surname}: old salary {old_salary}, increase {increase_amount}, new salary {self.salary}.")
 
     def __str__(self):
-        return f"Name: {self.name}\nPosition: {self.position}\nSalary: {self.salary}"
+        return f"Name: {self.name}\nSurname: {self.surname}\nPosition: {self.position}\nSalary: {self.salary}"
     
     @property
     def salary(self):
@@ -28,7 +39,7 @@ class Employee:
 
 
 def main():
-    e1 = Employee("Janko Mrkvicka", "Technician", 2000)
+    e1 = Employee("Janko", "Mrkvicka", "Technician", 2000)
 
     print(e1)
     e1.raise_salary(200)
