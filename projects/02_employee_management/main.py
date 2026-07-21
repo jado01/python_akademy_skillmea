53ffb729da950d20ac402f96cfbba387ca16917d
jado01 <jan.dorinsky@protonmail.com>
2026-07-21 11:23:47 +0200

    fix: restore log file path

--- projects/02_employee_management/main.py
+++ projects/02_employee_management/main.py
@@ -1,6 +1,9 @@
 import datetime
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
+LOG_FILE = BASE_DIR / "employee_management.log"
+
 def save_log(message):
     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(LOG_FILE, "a", encoding="UTF-8") as f:
