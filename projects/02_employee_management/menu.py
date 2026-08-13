ce8ab44fe9f89345d288cdc5ef82c1e7dc6cf4cc
jado01 <jan.dorinsky@protonmail.com>
2026-08-13 23:24:27 +0200

    refactor: extract organization menu actions

--- projects/02_employee_management/menu.py
+++ projects/02_employee_management/menu.py
@@ -2,7 +2,8 @@ from helpers import clear_terminal, pause, get_non_empty_input, select_departmen
 from models.organization import Department, Leader
 from services.data_storage import save_employees, load_employees, save_departments, load_departments
 from services.audit_log import save_log
-from actions import create_employee, show_all_employees, increase_employee_salary, create_manager
+from actions import add_employee_to_team, add_employee_to_department, create_employee, create_manager, create_department, show_departments, create_team
+from actions import increase_employee_salary, show_all_employees
 
 def run_menu():
     employees = load_employees()
@@ -42,134 +43,19 @@ def run_menu():
             create_manager(employees)
 
         elif choice == "5":
-            managers = []
-
-            for employee in employees:
-                if employee.employee_type == "manager" and employee.managed_department is None:
-                    managers.append(employee)
-
-            if not managers:
-                print("You need at least one manager to create a new department.")
-                pause()
-                continue
-            for manager in managers:
-                print(manager)
-
-            found_manager = None
-
-            while True:
-                try:
-                    manager_id = int(input("Enter ID of manager: "))
-                    for manager in managers:
-                        if manager.employee_id == manager_id:
-                            found_manager = manager
-                            break
-                except ValueError:
-                    print("ID must be a number!")
-                    pause()
-                    continue
-
-                if found_manager is None:
-                    print("There is no manager with this ID. Please enter ID again.")
-                else:
-                    while True:
-                        department_name = get_non_empty_input("Enter a department name: ")
-                        existing_department = None
-                        for department in departments:
-                            if department.name.lower() == department_name.lower():
-                                existing_department = department
-                                break
-                        if existing_department is not None:
-                            print("This department already exist. Please try again")
-                        else:
-                            new_department = Department(department_name, found_manager)
-                            departments.append(new_department)
-                            save_departments(departments)
-                            print(f"New department {department_name} with manager {found_manager.name} {found_manager.surname} created")
-                            break
-                    break
-            pause()
+            create_department(employees, departments)
 
         elif choice == "6":
-            if not departments:
-                print("No departments to show.")
-            else:
-                for department in departments:
-                    print(f"Department: {department.name}, Manager: {department.manager.name} {department.manager.surname}")
-            pause()
+            show_departments(departments)
 
         elif choice == "7":
-            if not departments:
-                print("You need at least one department before adding an employee.")
-            else:
-                available_employees = []
-                for employee in employees:
-                    if employee.employee_type == "employee" and employee.department is None:
-                        available_employees.append(employee)
-                if not available_employees:
-                    print("There are no employees available to add.")
-                else:
-                    found_department = select_department(departments)                                                
-                
-                    print(f"Which employee do you want to add to department {found_department.name}?")
-
-                    while True:
-                        chosen_employee = select_employee(available_employees)
-
-                        try:
-                            found_department.manager.add_employee_to_department(found_department, chosen_employee)
-                            save_departments(departments)
-                        except ValueError as error:
-                            print(error)
-                        else:
-                            print(f"Employee {chosen_employee.name} {chosen_employee.surname} added to department {found_department.name}")
-                            break
-            pause()
+            add_employee_to_department(employees, departments)
 
         elif choice == "8":
-            if not departments:
-                print("You need to create a department before creating a team.")
-            else:
-                found_department = select_department(departments)
-                
-                while True:
-                    team_name = get_non_empty_input("Please enter a team name: ")
-                    try:
-                        new_team = found_department.create_team(team_name)
-                    except ValueError as error:
-                        print(error)
-                    else:
-                        save_departments(departments)
-                        print(f"Team {new_team.name} was added to department {found_department.name}")
-                        print(found_department.list_teams())
-                        break
-            pause()
+            create_team(departments)
 
         elif choice == "9":
-            if not departments:
-                print("You need at least one department.")
-            else:
-                found_department = select_department(departments)
-
-                if not found_department.teams:
-                    print(f"Department {found_department.name} has no teams.")
-                else:
-
-                    if not found_department.employees:
-                        print(f"Department {found_department.name} has no employees.")
-                    else:
-                        found_team = select_team(found_department)
-                        chosen_employee = select_employee(found_department.employees)
-
-                        try:
-                            found_department.add_employee_to_team(chosen_employee, found_team)
-                        except ValueError as error:
-                            print(error)
-                        else:
-                            save_departments(departments)
-                            print(f"Employee {chosen_employee.name} {chosen_employee.surname} from department {found_department.name} was added to team {found_team.name}")
-                            print(found_team.list_members())
-            pause()
+            add_employee_to_team(departments)
 
         elif choice == "10":
             leaders = get_leaders(employees)
